0459cc478debff91020482648070b29645bf9b65
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-03-03 19:09:51 +0530

    Data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface HeroDetail {
+  name: string;
+  id: number | string;
+}
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HeroesComponent {
+  heroDetail: HeroDetail = { name: '', id: '' };
+  heroes: any[] = [
+    { id: 1, name: 'Wade' },
+    { id: 2, name: 'Drex' },
+    { id: 3, name: 'John' },
+    { id: 1, name: 'Reid' },
+  ];
+  getHeroDetail(hero: any): void {
+    console.log('HERO DETAIL', hero);
+    this.heroDetail.name = hero.name;
+    this.heroDetail.id = hero.id;
   }
-
 }
